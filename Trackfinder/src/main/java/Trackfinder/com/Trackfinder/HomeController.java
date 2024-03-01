9fa666b4f2e0dfacbf474edd01ede21a367ff627
baeyunhyeok <yunhyeok0928@gmail.com>
2024-03-01 14:31:28 +0900

    thymeleaf 공통 부분 묶기
    thymeleaf 공통 부분 묶기

--- Trackfinder/src/main/java/Trackfinder/com/Trackfinder/HomeController.java
+++ Trackfinder/src/main/java/Trackfinder/com/Trackfinder/HomeController.java
@@ -3,12 +3,14 @@ package Trackfinder.com.Trackfinder;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "home";
+        return "section1";
     }
 }
