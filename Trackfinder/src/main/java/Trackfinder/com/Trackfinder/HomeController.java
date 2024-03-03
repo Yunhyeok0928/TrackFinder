7fc2d0e11a866bf2f58e1b37dfd6f07bbcd85976
baeyunhyeok <yunhyeok0928@gmail.com>
2024-03-03 10:51:05 +0900

    section 파일분리 완료
    section 파일분리

--- Trackfinder/src/main/java/Trackfinder/com/Trackfinder/HomeController.java
+++ Trackfinder/src/main/java/Trackfinder/com/Trackfinder/HomeController.java
@@ -3,14 +3,13 @@ package Trackfinder.com.Trackfinder;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "section1";
+        return "section";
     }
+
 }
